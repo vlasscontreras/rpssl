3392d3e154b5b792c689ecd3deb99f0ff5646cea
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-01-29 15:26:33 -0600

    feat(modal): add the modal component

---
+++ components/dialogs/Modal.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="modal-wrapper fixed top-0 left-0 flex justify-center items-center w-full h-full" tabindex="-1" aria-modal="true" role="dialog">
+    <div class="backdrop fixed top-0 left-0 bg-black bg-opacity-30 w-full h-full" @click="close" />
+
+    <div class="modal bg-white rounded-xl px-8 pt-8 pb-12 relative flex flex-col justify-center items-center sm:items-start w-full h-full sm:h-auto">
+      <button class="absolute top-10 right-10 focus:outline-none" @click="close">
+        <Close />
+      </button>
+
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  methods: {
+    close () {
+      this.$emit('onClose')
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  @media (min-width: 640px) {
+    width: 420px;
+  }
+}
+</style>
