44c06b9ca8acf95f7905b124c87294eb627d0072
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-01-30 16:31:31 -0600

    feat(components): add the inverted button variation

--- components/Button.vue
+++ components/Button.vue
@@ -1,16 +1,58 @@
 <template>
-  <button class="border-2 border-header-outline hover:bg-white hover:border-white hover:text-dark focus:outline-none px-8 py-2 rounded-lg text-white text-xl transition-all uppercase table mx-auto sm:mr-0" v-on="listeners">
+  <button :class="classes" v-on="listeners">
     <slot />
   </button>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import classnames from 'classnames'
 
 export default Vue.extend({
+  props: {
+    inverted: {
+      type: Boolean,
+      default: false
+    }
+  },
+
   computed: {
     listeners () {
       return this.$listeners
+    },
+
+    classes (): String {
+      return classnames(
+        'border-2',
+        'focus:outline-none',
+        'mx-auto',
+        'px-8',
+        'py-2',
+        'rounded-lg',
+        'sm:mr-0',
+        'table',
+        'text-xl',
+        'transition-all',
+        'uppercase',
+
+        {
+          // Normal.
+          'bg-transparent': !this.inverted,
+          'hover:bg-white': !this.inverted,
+          'border-header-outline': !this.inverted,
+          'hover:border-white': !this.inverted,
+          'text-white': !this.inverted,
+          'hover:text-dark': !this.inverted,
+
+          // Inverted.
+          'bg-white': this.inverted,
+          'hover:bg-transparent': this.inverted,
+          'border-header-white': this.inverted,
+          'hover:border-header-outline': this.inverted,
+          'text-dark': this.inverted,
+          'hover:text-white': this.inverted
+        }
+      )
     }
   }
 })
