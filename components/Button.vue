b71c461beb73f89f46f6d6c1a60280c67a820990
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-01-30 15:33:03 -0600

    feat(components): add button component

---
+++ components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="border-2 border-header-outline hover:bg-white hover:border-white hover:text-dark focus:outline-none px-8 py-2 rounded-lg text-white text-xl transition-all uppercase table mx-auto sm:mr-0" v-on="listeners">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  computed: {
+    listeners () {
+      return this.$listeners
+    }
+  }
+})
+</script>
